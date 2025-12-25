74094d251458095d242e6ba315052bc9d66c52cb
SeinaruHi <vano.xmao@mail.ru>
2025-12-25 14:14:54 +0500

    add экранов и переходов

--- app/src/main/java/com/example/genialidad/MainActivity2.kt
+++ app/src/main/java/com/example/genialidad/MainActivity2.kt
@@ -1,23 +1,23 @@
 package com.example.genialidad
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.genialidad.databinding.ActivityMain2Binding
 import com.example.genialidad.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.textSkip.setOnClickListener {
-            startActivity(Intent(this, MainActivity2::class.java))
+            startActivity(Intent(this, MainActivity3::class.java))
         }
     }
 }
